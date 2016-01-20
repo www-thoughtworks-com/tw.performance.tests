a2733189cac8db3f0acddf0e507448a2ecdd0896
Satish Pokala <pokalasatish@gmail.com>
2016-01-20 16:45:46 +0000

    Satish Dan #2976 DRY up test urls

--- insights_performance_test.js
+++ insights_performance_test.js
@@ -1,20 +1,27 @@
 require('coffee-script')
 var Scenario = require('http-benchmark')
+var base_url = 'http://perf.webteam.thoughtworks.com';
 
 scenario = new Scenario()
 
-scenario
-  .get('http://perf.webteam.thoughtworks.com/insights')
-  .get('http://perf.webteam.thoughtworks.com/insights/technology')
-  .get('http://perf.webteam.thoughtworks.com/radar')
-  .get('http://perf.webteam.thoughtworks.com/radar/platforms')
-  .get('http://perf.webteam.thoughtworks.com/')
-  .get('http://perf.webteam.thoughtworks.com/careers')
-  .get('http://perf.webteam.thoughtworks.com/events')
-  .get('http://perf.webteam.thoughtworks.com/about-us')
-  .get('http://perf.webteam.thoughtworks.com/products')
-  .concurrency(8)
-  .actions(10)
+var paths = [
+  '/insights',
+  '/insights/technology',
+  '/radar',
+  '/radar/platforms',
+  '/', 
+  '/careers',
+  '/events',
+  '/about-us',
+  '/products'
+]
+
+paths.forEach(function(path) {
+  scenario.get(base_url + path)
+})
+
+scenario.concurrency(8)
+  .actions(3)
   .throttle(5000)
   .report()
   .start();
