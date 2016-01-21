c9108f7606df57b3c5e5733c2ab584b57e82223f
Karl Stoney <kstoney@thoughtworks.com>
2016-01-21 17:43:04 +0000

    Karl create bucket on sync

--- sync.sh
+++ sync.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
+BUCKET=${GO_PIPELINE_NAME:-performance.dev}
+
+echo 'Ensuring bucket is created'
+aws s3api create-bucket --bucket $BUCKET --region us-east-1
+
 echo 'Uploading latest results...'
-aws s3 cp results s3://go.tw.image.performance --recursive
+aws s3 cp results s3://$BUCKET --recursive
 
 echo 'Downloading previous 10 average results...'
-aws s3 cp s3://go.tw.image.performance/average results/average --recursive --page-size 10
+aws s3 cp s3://$BUCKET/average results/average --recursive --page-size 10
 
 echo 'Downloading previous 10 max results...'
-aws s3 cp s3://go.tw.image.performance/max results/max --recursive --page-size 10
+aws s3 cp s3://$BUCKET/max results/max --recursive --page-size 10
