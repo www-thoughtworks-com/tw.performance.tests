7ddd08579dea0cda6e670bef8be65a98adb7efde
Karl Stoney <kstoney@thoughtworks.com>
2016-01-21 19:50:00 +0000

    Karl Better sync down and up separation

--- sync.sh
+++ sync.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 set -e
-
+DIRECTION=$1
 BUCKET=${GO_PIPELINE_NAME:-performance.dev}
 
 echo 'Ensuring bucket is created'
 aws s3api create-bucket --bucket $BUCKET --region us-east-1
 
-echo 'Uploading latest results...'
-aws s3 cp results s3://$BUCKET --recursive
+if [ "$DIRECTION" == "--up" ]; then
+  echo 'Uploading latest results...'
+  aws s3 cp results s3://$BUCKET --recursive
+fi
 
-echo 'Downloading previous 10 average results...'
-aws s3 cp s3://$BUCKET/average results/average --recursive --page-size 10
+if [ "$DIRECTION" == "--down" ]; then
+  echo 'Downloading previous 10 average results...'
+  aws s3 cp s3://$BUCKET/average results/average --recursive --page-size 10
 
-echo 'Downloading previous 10 max results...'
-aws s3 cp s3://$BUCKET/max results/max --recursive --page-size 10
+  echo 'Downloading previous 10 max results...'
+  aws s3 cp s3://$BUCKET/max results/max --recursive --page-size 10
+fi
