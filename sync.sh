fe03a8ce5ee9270a189fca78421d9c5e21be861f
Karl Stoney <kstoney@thoughtworks.com>
2016-01-21 20:07:45 +0000

    Karl Fixing equality issue

--- sync.sh
+++ sync.sh
@@ -6,12 +6,12 @@ BUCKET=${GO_PIPELINE_NAME:-performance.dev}
 echo 'Ensuring bucket is created'
 aws s3api create-bucket --bucket $BUCKET --region us-east-1
 
-if [ "$DIRECTION" == "--up" ]; then
+if [ "$DIRECTION" = "--up" ]; then
   echo 'Uploading latest results...'
   aws s3 cp results s3://$BUCKET --recursive
 fi
 
-if [ "$DIRECTION" == "--down" ]; then
+if [ "$DIRECTION" = "--down" ]; then
   echo 'Downloading previous 10 average results...'
   aws s3 cp s3://$BUCKET/average results/average --recursive --page-size 10
 
