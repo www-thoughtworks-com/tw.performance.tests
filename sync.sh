828e9f1039af91f5fddcd97033b3a32cb87c8a30
Karl Stoney <kstoney@thoughtworks.com>
2016-01-21 15:32:17 +0000

    Karl #2976 Graph ten results, not twenty

--- sync.sh
+++ sync.sh
@@ -4,8 +4,8 @@ set -e
 echo 'Uploading latest results...'
 aws s3 cp results s3://go.tw.image.performance --recursive
 
-echo 'Downloading previous 20 average results...'
-aws s3 cp s3://go.tw.image.performance/average results/average --recursive --page-size 20
+echo 'Downloading previous 10 average results...'
+aws s3 cp s3://go.tw.image.performance/average results/average --recursive --page-size 10
 
-echo 'Downloading previous 20 max results...'
-aws s3 cp s3://go.tw.image.performance/max results/max --recursive --page-size 20
+echo 'Downloading previous 10 max results...'
+aws s3 cp s3://go.tw.image.performance/max results/max --recursive --page-size 10
