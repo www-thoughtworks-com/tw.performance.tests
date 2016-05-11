f2aeed8059ad3c60208165b08765bed5ef8aec61
Saranraj <saranraj@in-saranrajsekar.local>
2016-05-11 15:59:19 +0530

    Saran/Raji #Fix Increase the page load time for insights page

--- consistent_load.js
+++ consistent_load.js
@@ -20,7 +20,7 @@ var paths = [
   ['/clients', maxResponseTime],
   ['/services', maxResponseTime],
   ['/products', maxResponseTime],
-  ['/insights', 4000],
+  ['/insights', 10000],
   ['/insights/blogs?page=2', maxResponseTime],
   ['/insights/technology', maxResponseTime],
   ['/api/v1/insights/technology?page=2', maxResponseTime],
